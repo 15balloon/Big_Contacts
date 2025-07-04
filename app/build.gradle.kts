6eca9f48451065ef1a23ba97b7cae8514c6778aa
15balloon <jsh159197@gmail.com>
2025-07-05 00:01:29 +0900

    Release: ver1.1.1(3)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.l5balloon.bigcontacts"
         minSdk = 28
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
